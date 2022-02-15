a565ac5a2acc8ea270dcdaeb4ed41dabd938e6a5
Nirav Patel <maguskyros@gmail.com>
2022-02-15 17:43:51 -0600

    Updated the project for better pathing. Since the project will be deployed as as subdirectory I had to fix the pathing as it was hacky previously. It will now deploy properly based on whatever home page is setup as.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App style={{ overflowY: "scroll" }} />
   </BrowserRouter>,
   document.getElementById("root")
